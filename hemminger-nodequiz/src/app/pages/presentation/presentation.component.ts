d09d46b48780880caef5b1f1bbac63687705aff0
Andrew Hemminger <arhemminger@gmail.com>
2019-10-08 08:40:57 -0500

    Image fix for carousel

--- hemminger-nodequiz/src/app/pages/presentation/presentation.component.ts
+++ hemminger-nodequiz/src/app/pages/presentation/presentation.component.ts
@@ -18,20 +18,24 @@ import presentations from '../../../assets/data/presentations.json';
 })
 export class PresentationComponent implements OnInit {
   title = 'Quiz presentation carousel'
+  images: any;
 
-  images = []
-  presentationId: string;
-
- /*
-  getPresentation('id') {
-    return this.http.get('./assets/data/presentations.json');
-
+  constructor() {
+    this.images = [
+      {name: 'Quiz 101 slide 1', image: 'q101s1.JPG'},
+      {name: 'Quiz 101 slide 2', image: 'q101s2.JPG'},
+      {name: 'Quiz 101 slide 3', image: 'q101s3.JPG'},
+      {name: 'Quiz 101 slide 4', image: 'q101s4.JPG'},
+      {name: 'Quiz 101 slide 5', image: 'q101s5.JPG'},
+      {name: 'Quiz 101 slide 6', image: 'q101s6.JPG'},
+      {name: 'Quiz 101 slide 7', image: 'q101s7.JPG'},
+      {name: 'Quiz 101 slide 8', image: 'q101s8.JPG'},
+      {name: 'Quiz 101 slide 9', image: 'q101s9.JPG'},
+      {name: 'Quiz 101 slide 10', image: 'q101s10.JPG'},
+      {name: 'Quiz 101 slide 11', image: 'q101s11.JPG'},
+      {name: 'Quiz 101 slide 12', image: 'q101s12.JPG'}
+    ];
   }
-*/
-  constructor(){
-
-  }
-
 
   ngOnInit() {
   }
